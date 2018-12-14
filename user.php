a964d6790edf1b3e21da9f9b8255ed7ffe7e5d96
Cocentz <45829475+Cocentz@users.noreply.github.com>
2018-12-15 02:25:10 +0700

    Update user.php

--- user.php
+++ user.php
@@ -1,8 +1,8 @@
 <?php
 echo $putih."[$red!$putih]$red Warning !!!".$t;
-echo $putih."[$red!$putih]$kuning Ini adalah program ilegal".$t;
-echo $putih."[$red!$putih]$kuning Resiko sepenuhnya di tanggung pengguna".$t;
-$msgwe = $putih."[$biru>$putih]$red Subscribe$putih Channel Cocentz 😊".$t;
+echo $putih."[$ijo!$putih] Ini adalah program ilegal".$t;
+echo $putih."[$kuning!$putih] Resiko sepenuhnya di tanggung pengguna".$t;
+$msgwe = $putih."[$biru>$putih] Subscribe Channel Cocentz 😊".$t;
 $data= [
 'alianz.hamza','Hamzah Ismail',
         ];
