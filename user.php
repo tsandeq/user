6e6fcd0367959a3d141a12443b66063569f4e824
Cocentz <45829475+Cocentz@users.noreply.github.com>
2018-12-14 19:04:42 +0700

    Update user.php

--- user.php
+++ user.php
@@ -1,7 +1,7 @@
 <?php
 echo $putih."[\33[91m!\033[97m]$red warning !!!".$t;
-echo $putih."[\033[91m!\033[97m]\033[93m Ini adalah program ilegal".$t;
-echo $putih."[\033[91m!\033[97m]\033[93m Resiko sepenuhnya di tanggung pengguna".$t;
+echo $putih."[\033[93m!\033[97m]\033[93m Ini adalah program ilegal".$t;
+echo $putih."[\033[93m!\033[97m]\033[93m Resiko sepenuhnya di tanggung pengguna".$t;
 $data= [
 'alianz.hamza',
         ];
