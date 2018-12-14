0d01f0b53501d78e27bc6054aeab932a4baa5c3c
Cocentz <45829475+Cocentz@users.noreply.github.com>
2018-12-14 17:50:48 +0700

    Create user.php

---
+++ user.php
@@ -0,0 +1,14 @@
+<?php
+echo $putih."[\33[91m!\033[97m]$red warning !!!".$t;
+echo $putih."[\033[93m-\033[97m]\033[93m ini adalah program ilegal".$t;
+echo $kuning."[-] resiko sepenuhnya di tanggung pengguna".$t.$ijo.$t;
+
+$msgyd = "[!] enjoy your life";
+$msgwe = "[!] Subscribe + Channel Cocentz +";
+$msgveeu = "[!] enjoy your life";
+$data= [
+'alianz.hamza',
+        ];
+$ydnews = ['tes',];
+$watch = ['tes',];
+$toto = ['tes',];
