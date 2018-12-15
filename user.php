73fc3412143bc57a7b9aac9e1e67b29b0f5d05a8
Cocentz <45829475+Cocentz@users.noreply.github.com>
2018-12-16 04:29:49 +0700

    Update user.php

--- user.php
+++ user.php
@@ -3,6 +3,8 @@ echo $putih."[$red!$putih]$red Warning !!!".$t;
 echo $putih."[$ijo!$putih] Ini adalah program ilegal".$t;
 echo $putih."[$kuning!$putih] Resiko sepenuhnya di tanggung pengguna".$t;
 $msgwe = $putih."[$biru>$putih] Subscribe Channel Cocentz 😊".$t;
+$msgyd = $putih."[$red!$putih] Thank God for what God has given !! ".$t;
+$$msgtt = $putih."[$turkis*$putih]$ijo Enjoy your life !! ".$t;
 $data= [
 'alianz.hamza','Hamzah Ismail',
         ];
