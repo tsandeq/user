6a53fdd6f825d042ee3920a92e8853c0a78f3210
Cocentz <45829475+Cocentz@users.noreply.github.com>
2018-12-18 03:56:52 +0700

    Update user.php

--- user.php
+++ user.php
@@ -3,11 +3,14 @@ echo $putih."[$red!$putih]$red Warning !!!".$t;
 echo $putih."[$ijo!$putih] Ini adalah program ilegal".$t;
 echo $putih."[$kuning!$putih] Resiko sepenuhnya di tanggung pengguna".$t;
 $msgwe = $putih."[$biru>$putih] Subscribe Channel Cocentz 😊".$t;
-$msgyd = $putih."[$red!$putih] Thank God for what God has given !! ".$t;
+$msgyd = $putih."[$red!$putih] Update [$red v.1.2$putih ] Premium ".$t;
 $$msgtt = $putih."[$turkis*$putih]$ijo Enjoy your life !! ".$t;
 $data= [
+//w
 'alianz.hamza','Hamzah Ismail','Lembu Gaming',
+//y
+'Melin Syg','Kolek Cau','Channel Cocentz','
         ];
-$ydnews = ['tes',];
-$ttnews = ['tes',];
-$watch = ['tes',];
+$ydnews = ['tes','premium',];
+$watcg = ['tes',];
+
