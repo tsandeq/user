9cbd9b1ee60b8cd614804e6e81f27e823e3dbe23
Cocentz <45829475+Cocentz@users.noreply.github.com>
2018-12-18 13:07:15 +0700

    Update user.php

--- user.php
+++ user.php
@@ -5,6 +5,7 @@ echo $putih."[$kuning!$putih] Resiko sepenuhnya di tanggung pengguna".$t;
 $msgwe = $putih."[$biru>$putih] Subscribe Channel Cocentz 😊".$t;
 $msgyd = $putih."[$red!$putih] Update [$red v.1.2$putih ] Premium ".$t;
 $$msgtt = $putih."[$turkis*$putih] Enjoy Your Life !! ".$t;
+$msgads = $putih."[$biru>$putih] Subscribe Channel YT Channel Cocentz & adi bodhir 😁
 $data= [
 'alianz.hamza','Hamzah Ismail','Lembu Gaming',
 //y
