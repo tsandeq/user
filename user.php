e029f7cc580d84370789dbbcff9a7cf6a3bbe1df
Cocentz <45829475+Cocentz@users.noreply.github.com>
2019-01-07 17:24:18 +0700

    Update user.php

--- user.php
+++ user.php
@@ -2,18 +2,18 @@
 echo $putih."[$red!$putih]$red Warning !!!".$t;
 echo $putih."[$ijo!$putih] Ini adalah program ilegal".$t;
 echo $putih."[$kuning!$putih] Resiko sepenuhnya di tanggung pengguna".$t;
-$msgwe = $putih."[$biru>$putih] Subscribe Channel Cocentz 😊".$t;
-$msgyd = $putih."[$red!$putih] Update [$red v.1.2$putih ] Premium & Multi Account".$t;
-$msgtt = $putih."[*] Enjoy Your Life !! ".$t;
-$msgveeu = $putih."[*] Enjoy Your Life !! ".$t;
-$msgads = $putih."[$biru>$putih] Subscribe Channel YT Channel Cocentz & adi bordir 😁".$t;
+$msgwe=$putih."[$biru>$putih] Subscribe Channel Cocentz 😊".$t;
+$msgyd=$putih."[$red!$putih] Update [$red v.1.2$putih ] Premium & Multi Account".$t;
+$msgtt=$putih."[*] Enjoy Your Life !! ".$t;
+$msgveeu=$putih."[*] Enjoy Your Life !! ".$t;
+$msgads=$putih."[$biru>$putih] Subscribe Channel YT Channel Cocentz & adi bordir 😁".$t;
 $stat_dony=true;
 $st_dony=true;
-$yd_stat = false;
-$vip1= false;
-$weblock =[""];
-$ydblock = [""];
-$we_stat = true;
+$yd_stat=false;
+$vip1=false;
+$weblock=[""];
+$ydblock=[""];
+$we_stat=true;
 $data= [
  //w
 'Cocentz','alianz.hamza','Hamzah Ismail','Lembu Gaming','Cybertz HackrzAllx','Ozil Oy','Alianz Jr.',
