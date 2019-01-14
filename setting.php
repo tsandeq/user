1353a4f7e6a05e2519b2e53c654718a830b67ede
Cocentz <45829475+Cocentz@users.noreply.github.com>
2019-01-14 15:11:20 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -6,9 +6,9 @@ $baca_plus=$red."
 |  _ \ / _` |/ __/ _` |$kuning | |_) | | | | / __|$red
 | |_) | (_| | (_| (_| |$kuning |  __/| | |_| \__ \ $red
 |____/ \__,_|\___\__,_|$kuning |_|   |_|\__,_|___/$putih
-===================================================
-[ Creator by :$ijo Cocentz$putih | Kode :$ijo F2YPBIHf0h$putih ]".$t;
-
+-------------------------------------------
+[ Creator by :$ijo Cocentz$putih | Kode :$ijo F2YPBIHf0h$putih ]
+-------------------------------------------".$t;
 $msg_baca=$putih."[$red!$putih] Channel YouTube : Channel Cocentz 😉".$t;
 $msg_baca=$putih."[".$biru."-".$putih."] Kreasikan Copasmu, Jangan Copas Kreasikuasiku.!!".$t;
 $stat_baca=true;
