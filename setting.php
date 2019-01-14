10c596344a43a0cf619cdc49087a857447d357c3
Cocentz <45829475+Cocentz@users.noreply.github.com>
2019-01-14 14:58:30 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -1,11 +1,13 @@
 <?php
 /* setting bot */
 $baca_plus=$red."
-  ____                  $kuning  ____  _$red
- | __ )  __ _  ___ __ _ $kuning |  _ \| |_   _ ___$red
- |  _ \ / _` |/ __/ _` |$kuning | |_) | | | | / __|$red
- | |_) | (_| | (_| (_| |$kuning |  __/| | |_| \__ \$red
- |____/ \__,_|\___\__,_|$kuning |_|   |_|\__,_|___/$putih
+ ____                  $kuning  ____  _$red
+| __ )  __ _  ___ __ _ $kuning |  _ \| |_   _ ___$red
+|  _ \ / _` |/ __/ _` |$kuning | |_) | | | | / __|$red
+| |_) | (_| | (_| (_| |$kuning |  __/| | |_| \__ \$red
+|____/ \__,_|\___\__,_|$kuning |_|   |_|\__,_|___/$putih
+";
+echo$putih."[ Creator : Cocentz | Kode invite : F2YPBIHf0h | v.1 ]".$t;
 $msg_baca= "[!] Kreasikan Copasmu, Jangan Copas Kreasiku.!!";
 $stat_baca=true;
  
