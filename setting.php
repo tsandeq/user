06787195b23845f4fb72ee133f0c211333ffa990
Cocentz <45829475+Cocentz@users.noreply.github.com>
2019-01-14 15:02:14 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -4,9 +4,9 @@ $baca_plus=$red."
  ____                  $kuning  ____  _$red
 | __ )  __ _  ___ __ _ $kuning |  _ \| |_   _ ___$red
 |  _ \ / _` |/ __/ _` |$kuning | |_) | | | | / __|$red
-| |_) | (_| | (_| (_| |$kuning |  __/| | |_| \__ \$red
+| |_) | (_| | (_| (_| |$kuning |  __/| | |_| \__ \ $red
 |____/ \__,_|\___\__,_|$kuning |_|   |_|\__,_|___/$putih
-[ Creator :$ijo Cocentz$putih | Kode invite :ijo F2YPBIHf0h |$red v.1 ]".$t;
+[ Creator :$ijo Cocentz$putih | Kode :$ijo F2YPBIHf0h$putih ]".$t;
 $msg_baca= "[!] Kreasikan Copasmu, Jangan Copas Kreasiku.!!";
 $stat_baca=true;
  
