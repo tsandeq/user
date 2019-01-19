5fc4589202ae8f25a769480a22b2e09af441e115
Cocentz <45829475+Cocentz@users.noreply.github.com>
2019-01-19 21:36:25 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -25,11 +25,11 @@ Creator:$ijo Cocentz$putih | Kode:$ijo 3GRKE$putih | YT:$kuning Channel Cocentz$
  ░     ░     ░           ░       ░      ░ ░   ░    ░".$t;
 $msg_money=$putih."[~] Berkarya Selagi Bisa.... ".$t;
 $msg_cube=$putih."[^] Enjoy Your Life ".$t;
-$ccu= $red."
-──── ▄▀ ▄▀▄ ▀ █▄─█──── ▄▀ █── █─█ █▀▄────".$pth."
+$ccu=$red."
+──── ▄▀ ▄▀▄ ▀ █▄─█──── ▄▀ █── █─█ █▀▄────$pth
 ──── █─ █─█ █ █─▀█──── █─ █─▄ █─█ █▀█────
 ──── ─▀ ─▀─ ▀ ▀──▀──── ─▀ ▀▀▀ ─▀─ ▀▀─────
-creator: ".$ijo."Cocentz ".$pth."|| channel: ".$ijo."Channel Cocentz".$t;
+Creator :$ijo Cocentz | Kode Invit :$ijo TNQ7OH".$t;
 $msg_ccu=$ijo."[^] Official Yt Channel Cocentz";
 $stat_ccu=true;
 ?>
