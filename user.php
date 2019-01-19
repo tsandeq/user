297aedb5230fcb4b445249baf266288bf537ed4d
Cocentz <45829475+Cocentz@users.noreply.github.com>
2019-01-19 07:18:06 +0700

    Update user.php

--- user.php
+++ user.php
@@ -25,7 +25,7 @@ $data= [
 'Coki','behel.uje10@gmail.com','M.Khoirul Anwar','M. Khoirul Anwar','Belut Salmon','Selvi Ana','Syaifdin Joery',
  'Andri Mardat','KOCAK BGT','Papla Aqpkla','Muhammad pan','Irfan N','Becrotfr','Muhammad Khairil','Nando','Marsha',
  'Silva','Fardan','Nurdinn','C. DROID','Vika Cu','cocis','Gantari belantara putri','Reffer','Khomsun Bungsu',
- 'ria syafrianti',
+ 'ria syafrianti','Ahmad Rianto',
 '1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','22',
 '23','24','25','26','27','28','29','30',
  '1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','22',
