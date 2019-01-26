43182972eae9254a09d5c2fe70d795d9d4a552d0
Cocentz <45829475+Cocentz@users.noreply.github.com>
2019-01-26 16:59:38 +0700

    Update user.php

--- user.php
+++ user.php
@@ -1,12 +1,11 @@
 <?php
 echo $putih."[$red!$putih]$red Warning !!!".$t;
 echo $putih."[🇲🇨] Language Indonesia".$t;
-echo $putih."[$ijo!$putih] Ini adalah program ilegal".$t;
-echo $putih."[$kuning!$putih] Resiko sepenuhnya di tanggung pengguna".$t;
-echo $putih."[$red!$putih]$red Warning !!!".$t;
+echo $putih."[$red!$putih] Ini adalah program ilegal".$t;
+echo $putih."[$red!$putih] Resiko sepenuhnya di tanggung pengguna".$t;
 echo $putih."[🇬🇧] Language English".$t;
-echo $putih."[$ijo!$putih] This is an illegal program".$t;
-echo $putih."[$kuning!$putih] Risk fully borne by the user".$t;
+echo $putih."[$red!$putih] This is an illegal program".$t;
+echo $putih."[$red!$putih] Risk fully borne by the user".$t;
 
 $msgwe=$putih."[$red!$putih] Update News Script VIP$red v.2.0$putih".$t;
 $msgyd=$putih."[$red!$putih] Update [$red v.1.2$putih ] Premium & Multi Account".$t;
